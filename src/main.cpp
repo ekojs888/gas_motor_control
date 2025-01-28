0fa1e8a4bf6124233f42e0ce03e4c6d96ab65493
ekojs888 <ekojs999@gmail.com>
2025-01-28 23:57:27 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,20 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-const int mapPin = A0;  // Pin sensor MAP
-const int servoPin = 9; // Pin servo
+#define NUM_DATA 5
+
+const int mapPin = A0;    // Pin sensor MAP
+const int servoPin = SDA; // Pin servo
 Servo servo;
 
 const int noPressureValue = 72; // Nilai sensor tanpa tekanan
-int mapData[5][2] = {
-    {0, 20}, // Sensor 0 -> Servo 20%
-    {1, 25}, // Sensor 1 -> Servo 25%
-    {4, 30}, // Sensor 4 -> Servo 30%
-    {8, 40}, // Sensor 8 -> Servo 40%
-    {9, 50}  // Sensor 9 -> Servo 50%
+int actualValueLast = 0;
+int mapData[NUM_DATA][2] = {
+    {0, 0},  // Sensor 0 -> Servo 20%
+    {1, 5},  // Sensor 1 -> Servo 25%
+    {2, 10}, // Sensor 4 -> Servo 30%
+    {3, 20}, // Sensor 8 -> Servo 40%
+    {4, 30}  // Sensor 9 -> Servo 50%
 };
 
 int calculateActualValue(int pressure);
@@ -23,19 +26,24 @@ void setup()
   servo.attach(servoPin);
   servo.write(0); // Awal servo di posisi 0%
   Serial.println("System Ready");
+  delay(1000);
 }
 
 void loop()
 {
-  int sensorValue = analogRead(mapPin);                      // Membaca nilai sensor MAP
-  int pressure = map(sensorValue, 0, 1023, 0, 100);          // Mapping nilai sensor ke 0-100
+  int sensorValue = analogRead(mapPin);             // Membaca nilai sensor MAP
+  int pressure = map(sensorValue, 0, 1023, 0, 100); // Mapping nilai sensor ke 0-100
   int actualValue = calculateActualValue(pressure);          // Hitung nilai aktual
+  if (actualValueLast == actualValue)
+  {
+    return;
+  }
   int servoPosition = interpolateServoPosition(actualValue); // Hitung bukaan servo
 
   // Konversi persen bukaan ke derajat servo (0-180 derajat)
-  int servoAngle = map(servoPosition, 0, 100, 0, 180);
+  int servoAngle = map(servoPosition, 0, 100, 0, 120);
 
-  servo.write(servoAngle);  // Kirim posisi ke servo
+  servo.write(servoPosition); // Kirim posisi ke servo
   Serial.print("Sensor: ");
   Serial.print(sensorValue);
   Serial.print(" | Pressure: ");
@@ -48,7 +56,8 @@ void loop()
   Serial.print(servoAngle);
   Serial.println("°");
 
-  delay(500); // Delay pembacaan setiap 100 ms
+  actualValueLast = actualValue;
+  delay(200); // Delay pembacaan setiap 100 ms
 }
 
 // Fungsi untuk menghitung nilai aktual berdasarkan tekanan
@@ -60,14 +69,15 @@ int calculateActualValue(int pressure)
   }
   else
   {
-    return pressure - noPressureValue;
+    // return pressure - noPressureValue;
+    return 0;
   }
 }
 
 // Fungsi untuk menghitung bukaan servo berdasarkan interpolasi linear
 int interpolateServoPosition(int actualValue)
 {
-  for (int i = 0; i < 4; i++)
+  for (int i = 0; i < NUM_DATA; i++)
   {
     // Jika actualValue berada di antara dua nilai dalam tabel
     if (actualValue >= mapData[i][0] && actualValue <= mapData[i + 1][0])
